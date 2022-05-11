b647dac7d99ebf3ef49fbac7c16ab2b30d959a11
Enrico De Maria <enricodem35@gmail.com>
2022-05-11 11:56:34 +0200

    props-01

---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome, {this.props.name}</p>
+      </div>
+    );
+  }
+}
+export default Welcome;
