734a8b3e585780af223251383fcfed144dfafdc6
Enrico De Maria <enricodem35@gmail.com>
2022-05-12 19:20:48 +0200

    state-04

---
+++ src/CounterDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+class CounterDisplay extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.count}</h1>
+      </div>
+    );
+  }
+}
+export default CounterDisplay;
