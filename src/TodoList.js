0583870dc76d9ba4980b9e68d0784db82eace815
Enrico De Maria <enricodem35@gmail.com>
2022-05-14 18:15:39 +0200

    fix

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export class TodoList extends React.Component {
   state = {
-    names: ["Beautiful", "La Casa de Papel", "Fratelli d'Italia", "pinocchio"],
+    names: ["Beautiful", "La Casa de Papel", "Fratelli d'Italia", "Pinocchio"],
   };
 
   handleAddTodo = (event) => {
