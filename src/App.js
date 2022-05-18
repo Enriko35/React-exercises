73ef8364780011950b9381d34af409026a6fa127
Enriko35 <enricodem35@gmail.com>
2022-05-18 17:44:25 +0200

    useState-02

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ClickCounter from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import Hello from "./Hello";
 import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
+import  Login  from "./Login";
 import { TodoList } from "./TodoList";
 import { UncontrolledLogin } from "./UncontrolledLogin";
 import { Container } from "./Container";
--- src/Login.js
+++ src/Login.js
@@ -1,71 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class Login extends React.Component {
-  state = {
+const Login = () => {
+  const [data, setData] = useState({
     username: "",
     password: "",
     remember: false,
-  };
+  });
 
-  handleInputChange = (event) => {
-    const value = event.target.value;
-    const name = event.target.name;
-    const type = event.target.type;
-    const checked = event.target.checked;
+  function handleInputChange(event) {
+    const { name, type, value, checked } = event.target;
 
-    this.setState({
+    setData({
       [name]: type === "checkbox" ? checked : value,
     });
-  };
-
-  handleLogin = () => {
-    this.setState({
-      username: "eric",
-      password: "world33",
-      remember: true,
-    });
-  };
-
-  handleResetState = () => {
-    this.setState({
-      username: "",
-      password: "",
-      remember: false,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <div>
-          <input
-            name="username"
-            value={this.state.username}
-            onChange={this.handleInputChange}
-          />
-          <input
-            name="password"
-            type="password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-          <input
-            name="remember"
-            type="checkbox"
-            checked={this.state.remember}
-            onChange={this.handleInputChange}
-          />
-          <button
-            style={{
-              backgroundColor: this.state.password.length < 8 ? "red" : "green",
-            }}
-            onClick={this.handleLogin}
-          >
-            Invio
-          </button>
-          <button onClick={this.handleResetState}>Reset</button>
-        </div>
-      </div>
-    );
   }
-}
+
+  return (
+    <form>
+      <input
+        onChange={handleInputChange}
+        value={data.username}
+        name="username"
+      />
+      <input
+        onChange={handleInputChange}
+        type="password"
+        value={data.password}
+        name="password"
+      />
+      <input
+        onChange={handleInputChange}
+        checked={data.remember}
+        type="checkbox"
+        name="remember"
+      />
+    </form>
+  );
+};
+
+export default Login;
