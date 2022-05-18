2594567693bfe3ca61afeffd0b5cb88d2e3a9fc7
Enriko35 <enricodem35@gmail.com>
2022-05-18 18:20:35 +0200

    useEffect-01

--- src/Counter.js
+++ src/Counter.js
@@ -1,35 +1,15 @@
-// import React from "react";
-// import CounterDisplay from "./CounterDisplay";
-
-// class Counter extends React.Component {
-//   state = {
-//     count: this.props.initialCounter,
-//   };
-//   componentDidMount() {
-//     setInterval(() => {
-//       this.setState({
-//         count: this.state.count + this.props.increment,
-//       });
-//     }, this.props.time);
-//   }
-//   render() {
-//     return <CounterDisplay count={this.state.count} />;
-//   }
-// }
-
-// Counter.defaultProps = {
-//   initialCounter: 0,
-//   increment: 1,
-//   time: 1000,
-// };
-// export default Counter;
-
-import React, { useState } from 'react'
+import React, {useEffect, useState } from 'react'
 
 const Counter = ({initialValue = 0}) => {
 const [counter, setCounter] = useState(initialValue)
 
-function handleCocunterIncrement() {
+useEffect(() => {
+console.log(`Counter start in: ${counter}`)
+}, [counter])
+
+
+
+function onCounterChange() {
   setCounter(counter + 1)
 
 }
@@ -37,7 +17,7 @@ function handleCocunterIncrement() {
   return (
     <div>
       <h2>Counter: {counter}</h2>
-      <button onClick={handleCocunterIncrement}>Increment</button>
+      <button onClick={onCounterChange}>Increment</button>
     </div>
   )
 }
