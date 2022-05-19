ea485692853c488d63295e4e2461b1f7e7fa57ad
Enriko35 <enricodem35@gmail.com>
2022-05-19 20:09:28 +0200

    Custom-hooks-04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,14 +1,19 @@
-import {useGithubUser} from "./useGithubUser";
+import { useGithubUser } from "./useGithubUser";
 
-const GithubUser = ({username}) => {
-  const {data} = useGithubUser(username)
-return <div>
-      {data &&<h1>{data.name}</h1> }
-  </div>;
+const GithubUser = ({ username }) => {
+  const { data, load, err, onFetchUser } = useGithubUser(username);
 
+  function handleGetUserData() {
+    onFetchUser(username);
+  }
 
+  return (
+    <div>
+      <button onClick={handleGetUserData}> Load</button>
+      {load && <h2>load...</h2>}
+      {err && <h2>There is an error</h2>}
+      {data && <h1>{data.name}</h1>}
+    </div>
+  );
 };
-
-
-
 export default GithubUser;
