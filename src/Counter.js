b57b90f9195f97a3a6dee2dca0477c49b8ecdc3b
Enriko35 <enricodem35@gmail.com>
2022-05-19 18:57:13 +0200

    Custom-hooke-01

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const Counter = ({ initialValue = 0 }) => {
+function useCounter(initialValue = 0) {
   const [counter, setCounter] = useState(initialValue);
 
-  useEffect(() => {
-    console.log(`Counter start in: ${counter}`);
-  }, [counter]);
-
-  function onCounterChange() {
-    setCounter(counter + 1);
+  function handleCounterIncrement() {
+    setCounter((c) => c + 1);
   }
+
+  function handlerCounterDecrement() {
+    setCounter((c) => c - 1);
+  }
+
+  function handlerCounterReset() {
+    setCounter(initialValue);
+  }
+
+  return {
+    counter: counter,
+    onIncrement: handleCounterIncrement,
+    onDecrement: handlerCounterDecrement,
+    onReset: handlerCounterReset,
+  };
+}
+
+const Counter = ({ initialValue = 0 }) => {
+  const { counter, onIncrement, onDecrement, onReset } =
+    useCounter(initialValue);
 
   return (
     <div>
       <h2>Counter: {counter}</h2>
-      <button onClick={onCounterChange}>Increment</button>
+
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+      <button onClick={onReset}>Reset</button>
     </div>
   );
 };
