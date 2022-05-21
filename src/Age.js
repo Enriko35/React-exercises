73253cbebb2b447ad2a9d91f870da0ed3e3a0d58
Enrico <95548403+Enriko35@users.noreply.github.com>
2022-05-21 16:05:14 +0200

    Update Age.js

--- src/Age.js
+++ src/Age.js
@@ -4,7 +4,7 @@ class Age extends React.Component {
   render() {
     return (
       <div>
-        <p>Your age is:{this.props.age > 18 ? this.props.age : false}</p>
+        <p>Your age is:{this.props.age}</p>
       </div>
     );
   }
