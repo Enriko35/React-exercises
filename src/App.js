5472c639b3e6182a3acd942eecf35db26609b2b8
Enriko35 <enricodem35@gmail.com>
2022-05-23 12:47:38 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-import ClickCounter from "./ClickCounter";
-import { ClickTracker } from "./ClickTracker";
-import Hello from "./Hello";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import Login from "./Login";
-import { TodoList } from "./TodoList";
-import { UncontrolledLogin } from "./UncontrolledLogin";
-import { Container } from "./Container";
-// import { DisplayLenguage } from "./DisplayLenguage";
-import { Language } from "./Language";
-import Welcome from "./Welcome";
-import Sum from "./Sum";
-import Counter from "./Counter";
-import GithubUserList from "./GithubUserList";
-import FilteredList from "./FilteredList";
-import CarDetails from "./CarDetails";
-=======
 import { DisplayLanguage } from "./DisplayLanguage";
 import { LanguageContext } from "./LanguageContext";
->>>>>>> useContext
 
 export function App() {
   const [language, setLanguage] = useState("it");
@@ -34,62 +15,10 @@ export function App() {
           <option value="it">Italiano</option>
         </select>
 
-<<<<<<< HEAD
-          {/* <DisplayLenguage.Provider value={this.state.language}> */}
-          <Welcome name="eric" />
-          <Language />
-          <GithubUserList />
-          <CarDetails
-            initialData={{
-              model: "",
-              year: "",
-              color: "",
-            }}
-          />
-          <FilteredList />
-          <Hello />
-          <Sum />
-          <InteractiveWelcome />
-          <ClickCounter />
-          <ClickTracker />
-          <Login />
-          <UncontrolledLogin />
-          <TodoList
-            render={(names, addTodo, reset, removeTodo) => {
-              return (
-                <div>
-                  <form onSubmit={addTodo}>
-                    <input name="todo" type="text" />
-                    <button type="submit">Submit</button>
-                    <button onClick={reset}>Reset</button>
-                  </form>
-                  <ul>
-                    {names.map((item, index) => {
-                      return (
-                        <div>
-                          <li>
-                            {item}
-                            <button onClick={() => removeTodo(index)}>x</button>
-                          </li>
-                        </div>
-                      );
-                    })}
-                  </ul>
-                </div>
-              );
-            }}
-          />
-          {/* </DisplayLenguage.Provider> */}
-        </div>
-        {showCounter && <Counter />}
-        <button onClick={handleToggleCounter}>Toggle counter</button>
-      </Container>
-=======
         <LanguageContext.Provider value={language}>
           <DisplayLanguage />
         </LanguageContext.Provider>
       </div>
->>>>>>> useContext
     </div>
   );
 }
