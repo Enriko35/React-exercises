e4e6b4bb7b023952ae27a7458292641155b5f650
Enriko35 <enricodem35@gmail.com>
2022-05-23 12:45:01 +0200

    useContext

--- src/App.js
+++ src/App.js
@@ -1,90 +1,24 @@
 import React, { useState } from "react";
-import ClickCounter from "./ClickCounter";
-import { ClickTracker } from "./ClickTracker";
-import Hello from "./Hello";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import Login from "./Login";
-import { TodoList } from "./TodoList";
-import { UncontrolledLogin } from "./UncontrolledLogin";
-import { Container } from "./Container";
-// import { DisplayLenguage } from "./DisplayLenguage";
-import { Language } from "./Language";
-import Welcome from "./Welcome";
-import Sum from "./Sum";
-import Counter from "./Counter";
-import GithubUserList from "./GithubUserList";
-import FilteredList from "./FilteredList";
+import { DisplayLanguage } from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext";
 
 export function App() {
-  // state = {
-  //   language: "it",
-  // };
-
-  // handleLenguageChange = (event) => {
-  //   this.setState({
-  //     language: event.target.value,
-  //   });
-  // };
-
-  const [showCounter, setShowCounter] = useState(true);
-  function handleToggleCounter() {
-    setShowCounter((s) => !s);
-  }
-
+  const [language, setLanguage] = useState("it");
+  const handleLanguage = (event) => {
+    setLanguage(event.target.value);
+  };
   return (
     <div>
-      <Container title={"React-lesson"}>
-        <div>
-          {/* <select
-              value={this.state.language}
-              onChange={this.handleLenguageChange}
-            >
-              <option value="en">English</option>
-              <option value="it">Italiano</option>
-            </select> */}
+      <div>
+        <select value={language} onChange={handleLanguage}>
+          <option value="en">English</option>
+          <option value="it">Italiano</option>
+        </select>
 
-          {/* <DisplayLenguage.Provider value={this.state.language}> */}
-          <Welcome name="eric" />
-          <Language />
-          <GithubUserList />
-          <FilteredList />
-          <Hello />
-          <Sum />
-          <InteractiveWelcome />
-          <ClickCounter />
-          <ClickTracker />
-          <Login />
-          <UncontrolledLogin />
-          <TodoList
-            render={(names, addTodo, reset, removeTodo) => {
-              return (
-                <div>
-                  <form onSubmit={addTodo}>
-                    <input name="todo" type="text" />
-                    <button type="submit">Submit</button>
-                    <button onClick={reset}>Reset</button>
-                  </form>
-                  <ul>
-                    {names.map((item, index) => {
-                      return (
-                        <div>
-                          <li>
-                            {item}
-                            <button onClick={() => removeTodo(index)}>x</button>
-                          </li>
-                        </div>
-                      );
-                    })}
-                  </ul>
-                </div>
-              );
-            }}
-          />
-          {/* </DisplayLenguage.Provider> */}
-        </div>
-        {showCounter && <Counter />}
-        <button onClick={handleToggleCounter}>Toggle counter</button>
-      </Container>
+        <LanguageContext.Provider value={language}>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
+      </div>
     </div>
   );
 }
--- src/DisplayLenguage.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const DisplayLenguage = createContext("it");
