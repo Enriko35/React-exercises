0071704731a0e4c13b90757af5310b854a476981
Enriko35 <enricodem35@gmail.com>
2022-05-24 19:04:11 +0200

    react-router-04

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Counter from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
+import { Container } from "./Container";
 
 export function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/Welcome" element={<Welcome name="Erik" />} />
-          <Route path="/Counter" element={<Counter />} />
-          <Route path="/username" element={<ShowGithubUser />} />
-        </Routes>
+        <Container>
+          <div>
+            <Link to="/Welcome">Welcome</Link> |<Link to="/username">Name</Link>
+            |<Link to="/Counter">Counter</Link>
+          </div>
+          <Routes>
+            <Route path="/Welcome" element={<Welcome name="Erik" />} />
+            <Route path="/Counter" element={<Counter />} />
+            <Route path="/username" element={<ShowGithubUser />} />
+          </Routes>
+        </Container>
       </BrowserRouter>
     </>
   );
