2d24cf5fb266ff56dbe84f6ebeb8fde0fe7d1d55
Enriko35 <enricodem35@gmail.com>
2022-05-25 18:19:46 +0200

    swr-03

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -4,10 +4,18 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 function UseGithubUser() {
-  const { data, error } = useSWR(`https://api.github.com/users`, fetcher);
+  const { data, error, mutate } = useSWR(
+    `https://api.github.com/users`,
+    fetcher
+  );
+
+  function handleRefresh() {
+    mutate();
+  }
 
   return (
     <div>
+      <button onClick={handleRefresh}>Refresh</button>
       {!data && !error && <h3>Loading...</h3>}
       {error && <h3>An error has occurred</h3>}
       {data && !error && (
