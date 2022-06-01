13573d975692f93fd9e269361e674b131f7b746f
Enriko35 <enricodem35@gmail.com>
2022-06-01 10:23:32 +0200

    Redux-04

--- src/state/Store.js
+++ src/state/Store.js
@@ -1,10 +1,11 @@
 import { combineReducers, createStore } from "redux";
-
 import { counterState } from "./CounterState";
+
 import { todoReducer } from "./TodosReducers";
 
 const rootReducer = combineReducers({
   counter: counterState.reducer,
+
   todos: todoReducer.reducer,
 });
 
